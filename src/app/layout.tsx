5045ced0f758c860ca018c7d645f9b4de9903d1c
Patrick Düggelin <patrick.dueggelin@live.de>
2024-10-29 10:15:00 +0100

    :lipstick: Update app with info

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,9 +2,9 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "Demo — Pathfinding based on Classified Avalanche Terrain",
+  title: "Off-Grid: Safe Ski Tour Planning",
   description:
-    "Find the path with the lowest risk for avalanches between two points in Switzerland.",
+    "Explore Switzerland safely with Off-Grid's intelligent pathfinding tool, optimized for low-risk routes in classified avalanche terrain.",
 };
 
 export default function RootLayout({
@@ -14,7 +14,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className="bg-white dark:bg-gray-900 text-gray-900 dark:text-gray-100">
+        {children}
+      </body>
     </html>
   );
 }
